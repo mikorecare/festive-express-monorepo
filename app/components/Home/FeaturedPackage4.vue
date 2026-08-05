70aae9066b4c195beda5f62fe5dd4ae9df57c0ef
Roniefil Porquiado <rporquiado@FLPpros.com>
2026-08-06 04:08:59 +0800

    Package 2 Updates 1

--- app/components/Home/FeaturedPackage4.vue
+++ app/components/Home/FeaturedPackage4.vue
@@ -185,12 +185,12 @@ const getPackageIcon = (name: string) => {
 }
 
 // Static for Placeholder Temp
-const getPackagePhoto = (name: string) => {
-  const n = name.toLowerCase()
-  if (n.includes('jolly')) return `${BASE}/JollyPhoto.webp`
-  if (n.includes('merry')) return `${BASE}/MERRYphoto.webp`
-  return `${BASE}/JoyPhoto.webp`
-}
+// const getPackagePhoto = (name: string) => {
+//   const n = name.toLowerCase()
+//   if (n.includes('jolly')) return `${BASE}/JollyPhoto.webp`
+//   if (n.includes('merry')) return `${BASE}/MERRYphoto.webp`
+//   return `${BASE}/JoyPhoto.webp`
+// }
 
 </script>
 
