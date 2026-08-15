0e9a7131101f243a8ba6c714bb526b580c465736
miko <mikorecare@gmail.com>
2026-08-16 00:06:27 +0800

    FIX: deployment errors

--- apps/admin/app/components/AdminSidebar.vue
+++ apps/admin/app/components/AdminSidebar.vue
@@ -6,7 +6,7 @@
     <!-- Logo -->
     <div class="p-6 flex items-center justify-center flex-shrink-0">
       <img
-        src="/Images/FLP-Express-Transparent.PNG"
+        src="/Images/FLP-Express-Transparent.png"
         alt="FLP Admin"
         class="max-h-20 w-auto my-4 block"
       />
