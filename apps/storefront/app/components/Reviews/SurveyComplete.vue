b168a1e6b8e36f8e0804a1fe7d6bcdeaf18dc75d
miko <mikorecare@gmail.com>
2026-08-27 12:14:42 +0800

    FEAT: Updated survey complete screen

--- apps/storefront/app/components/Reviews/SurveyComplete.vue
+++ apps/storefront/app/components/Reviews/SurveyComplete.vue
@@ -1,6 +1,11 @@
 <template>
-  <div class="max-w-7xl mx-auto border border-4 !border-[#F39124] rounded-xl">
-    FESTIVE
+  <div class="flex flex-col items-center justify-center text-center">
+    <img
+      src="/Images/reviews/survey-done.png"
+      alt="Survey Complete"
+      class="mb-8 max-w-4xl w-full h-auto"
+    />
+
     <NuxtLink
       to="/"
       class="inline-block bg-[#F49321] text-white font-bold py-3 px-6 rounded-xl hover:bg-[#e07e0e] transition-colors no-underline"
