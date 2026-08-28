30fea5fd028247af74c367be8fcd95173ff0f07f
miko <mikorecare@gmail.com>
2026-08-29 02:29:07 +0800

    FIX: Redirection

--- apps/admin/server/api/auth/azure.get.ts
+++ apps/admin/server/api/auth/azure.get.ts
@@ -4,8 +4,6 @@ export default defineEventHandler((event) => {
   const tenantId = (config.azureTenantId as string) || 'common'
   const clientId = config.azureClientId as string
   const redirectUri = encodeURIComponent((config.azureRedirectUri as string) || '')
-
-  // Request basic profile and email scopes
   const scope = encodeURIComponent('openid profile email')
 
   const authUrl = `https://login.microsoftonline.com/${tenantId}/oauth2/v2.0/authorize?` +
