1848d8783408979cdfb93e1c60d6af3abf7f81ac
miko <mikorecare@gmail.com>
2026-09-06 11:22:50 +0800

    FIX: EC2 error

--- apps/storefront/app/components/PreviewYourHomeButton.vue
+++ apps/storefront/app/components/PreviewYourHomeButton.vue
@@ -3,18 +3,15 @@
     to="/preview-your-home"
     class="group relative z-10 block w-fit transition-transform duration-200 hover:-translate-y-0.5 mx-auto"
   >
-    <div class="relative w-[320px] max-lg:w-[180px] h-[320px] max-lg:h-[180px]">
-      <NuxtImg
+    <div
+      class="relative w-[320px] max-lg:w-[180px] h-[320px] max-lg:h-[180px] animate-pulse-image"
+    >
+      <img
         :src="frameSrc"
         alt="Preview Your Home"
-        class="absolute inset-0 z-0 h-full w-full object-contain drop-shadow-[0_12px_28px_rgba(0,0,0,0.2)] transition-opacity duration-300"
-        fit="contain"
+        class="absolute inset-0 z-0 h-full w-full object-contain drop-shadow-[0_12px_28px_rgba(0,0,0,0.2)]"
         loading="eager"
         draggable="false"
-        :img-attrs="{
-          style:
-            'display: block; width: 100%; height: 100%; object-fit: contain;',
-        }"
       />
     </div>
   </NuxtLink>
@@ -24,16 +21,29 @@
 const FRAME_A = "/Images/Festivo/PreviewYourHomeButton.png";
 const FRAME_B = "/Images/Festivo/PreviewYourHomeButton2.png";
 
-const showB = ref(false);
-const frameSrc = computed(() => (showB.value ? FRAME_B : FRAME_A));
+const frameSrc = ref(FRAME_A);
 
-let timer;
 onMounted(() => {
-  timer = setInterval(() => {
-    showB.value = !showB.value;
+  let showB = false;
+  setInterval(() => {
+    showB = !showB;
+    frameSrc.value = showB ? FRAME_B : FRAME_A;
   }, 500);
 });
-onUnmounted(() => {
-  if (timer) clearInterval(timer);
-});
 </script>
+
+<style scoped>
+.animate-pulse-image {
+  animation: softPulse 2s ease-in-out infinite;
+}
+
+@keyframes softPulse {
+  0%,
+  100% {
+    transform: scale(1);
+  }
+  50% {
+    transform: scale(1.02);
+  }
+}
+</style>
